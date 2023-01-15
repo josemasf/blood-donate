6bbc0a702e3eb13433ab2edb9d7c498ae9121bc8
Jose Maria Santos <innovation56@iberostar.com>
2023-01-15 12:30:07 +0100

    add scrape cron job

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
 import status from "../db/blood-status.json";
 import { Hono } from "hono";
+import { poweredBy } from "hono/powered-by";
 
 import { cors } from "hono/cors";
 
 const app = new Hono();
 app.use(cors({ origin: "*" }));
 
+app.use("*", poweredBy());
+
 app.get("/status", (ctx) => ctx.json(status));
-app.get("/", (ctx) => ctx.json("hola"));
+app.get("/status/:type", (ctx) => {
+  const foundStatusbyType = status.find(
+    (statu) => statu.type === ctx.req.param("type")
+  );
+  return foundStatusbyType
+    ? ctx.json(foundStatusbyType)
+    : ctx.json({ message: `Type ${ctx.req.param("type")} not found` }, 404);
+});
+app.get("/", (ctx) => {
+  return ctx.text("Hello Hono!");
+});
+
+export default app;
--- src/index.test.js
+++ src/index.test.js
@@ -2,23 +2,23 @@ import { unstable_dev } from "wrangler";
 import { describe, expect, it, beforeAll, afterAll } from "vitest";
 
 describe("Worker", () => {
-	let worker;
+  let worker;
 
-	beforeAll(async () => {
-		worker = await unstable_dev("src/index.js", {
-			experimental: { disableExperimentalWarning: true },
-		});
-	});
+  beforeAll(async () => {
+    worker = await unstable_dev("src/index.js", {
+      experimental: { disableExperimentalWarning: true },
+    });
+  });
 
-	afterAll(async () => {
-		await worker.stop();
-	});
+  afterAll(async () => {
+    await worker.stop();
+  });
 
-	it("should return Hello World", async () => {
-		const resp = await worker.fetch();
-		if (resp) {
-			const text = await resp.text();
-			expect(text).toMatchInlineSnapshot(`"Hello World!"`);
-		}
-	});
+  it("should return Hello Hono", async () => {
+    const resp = await worker.fetch();
+    if (resp) {
+      const text = await resp.text();
+      expect(text).toMatchInlineSnapshot(`"Hello Hono!"`);
+    }
+  });
 });
