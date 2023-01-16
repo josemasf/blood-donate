31a71e7e7a0b4ad6d7a47da224375d690633110a
Jose Maria Santos <innovation56@iberostar.com>
2023-01-16 19:54:50 +0100

    add last update data endpoint

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import status from "../db/blood-status.json";
+import update from "../db/blood-update.json";
 import { Hono } from "hono";
 import { poweredBy } from "hono/powered-by";
 
@@ -8,6 +9,8 @@ const app = new Hono();
 app.use(cors({ origin: "*" }));
 
 app.use("*", poweredBy());
+
+app.get("/update", (ctx) => ctx.json(update));
 
 app.get("/status", (ctx) => ctx.json(status));
 app.get("/status/:type", (ctx) => {
@@ -19,7 +22,24 @@ app.get("/status/:type", (ctx) => {
     : ctx.json({ message: `Type ${ctx.req.param("type")} not found` }, 404);
 });
 app.get("/", (ctx) => {
-  return ctx.text("Hello Hono!");
+  return ctx.json([
+    {
+      endpoint: "/status",
+      description: "Returns status of the blood bank of Córdoba Spain",
+      parameters: [
+        {
+          name: "type",
+          endpoint: "/status/:type",
+          description:
+            "Returns status of the blood bank of Córdoba Spain by type of blood",
+        },
+      ],
+    },
+    {
+      endpoint: "/update",
+      description: "Returns the date of last update of the information",
+    },
+  ]);
 });
 
 export default app;
